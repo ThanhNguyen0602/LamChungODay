48e72a92a1fed090f55b1ab2332de50c5b450230
ThanhNguyen0602 <thanh.httl2000@gmail.com>
2024-05-21 14:46:43 +0700

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'process';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot('mongodb://localhost:27017/'), 
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      uri: configService.get<string>('MONGO_URL'),
+    }),
+    inject: [ConfigService],
+  }),
+  
+  ConfigModule.forRoot({
+    ignoreEnvFile: true
+  }),
+  
+  UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+require ('dotenv').config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public')); //js, css, images
   app.setBaseViewsDir(join(__dirname, '..', 'views')); //view
   app.setViewEngine('ejs');
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
